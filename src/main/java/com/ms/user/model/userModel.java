6c12b1e62838eca5698beb7cee82849384be0d06
felipe steffan <fehsteffan@gmail.com>
2026-01-22 21:05:54 -0300

    Model user

---
+++ src/main/java/com/ms/user/model/userModel.java
@@ -0,0 +1,52 @@
+package com.ms.user.model;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USERS")
+public class userModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String cpf;
+    private String email;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
