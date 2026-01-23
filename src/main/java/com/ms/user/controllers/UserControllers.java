6bac4b4bde83af0ce275734d14d7b69dfa7c49d6
felipe steffan <fehsteffan@gmail.com>
2026-01-22 22:29:53 -0300

    UserService

--- src/main/java/com/ms/user/controllers/UserControllers.java
+++ src/main/java/com/ms/user/controllers/UserControllers.java
@@ -3,6 +3,7 @@ package com.ms.user.controllers;
 
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.model.UserModel;
+import com.ms.user.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
@@ -17,11 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class UserControllers {
 
+    final UserService userService;
+
+    public UserControllers(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDto, userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body();
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
     }
 
 
